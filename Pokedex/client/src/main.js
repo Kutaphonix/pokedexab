51cc4ddf256d45a4ab514c1c3e5e9c511aac4454
Kutaphonix <shofertm@gmail.com>
2026-04-28 22:18:57 +0200

    Update main.js

--- Pokedex/client/src/main.js
+++ Pokedex/client/src/main.js
@@ -4,17 +4,11 @@ import router from './router'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
 
-// Import stylów
+// Import tylko podstawowych stylów i ikon (komponenty wciągną się same)
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-// Tworzymy instancję Vuetify z ręcznie wstrzykniętymi wszystkimi komponentami
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+const vuetify = createVuetify() // Pusta instancja, bez "components" i "directives"
 
 const app = createApp(App)
 const pinia = createPinia()
