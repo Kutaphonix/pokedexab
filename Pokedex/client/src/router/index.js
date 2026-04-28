c2b0e3fe2f0a3d32d68fa9181cc806699fbf0aab
PSTorcik <154635027+PSTorcik@users.noreply.github.com>
2026-04-28 22:45:30 +0200

    Update index.js

--- Pokedex/client/src/router/index.js
+++ Pokedex/client/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PokemonDetails from '../views/PokemonDetails.vue'
-import AddView from '../views/AddView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
