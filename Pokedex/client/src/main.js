54cd0f2a2937b4d3638e27ef72fc5e1065287071
PSTorcik <154635027+PSTorcik@users.noreply.github.com>
2026-04-28 22:42:11 +0200

    Update main.js

--- Pokedex/client/src/main.js
+++ Pokedex/client/src/main.js
@@ -1,19 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-
-const vuetify = createVuetify()
+import { registerPlugins } from './plugins'
 
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia)
-app.use(router)
-app.use(vuetify)
+registerPlugins(app)
 
 app.mount('#app')
