4e1b7e4a5cac0a42f06609a4b0b837468e64edce
Kutaphonix <shofertm@gmail.com>
2026-04-28 21:16:56 +0200

    Update vite.config.js

--- Pokedex/client/vite.config.js
+++ Pokedex/client/vite.config.js
@@ -1,50 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-import Vue from '@vitejs/plugin-vue'
-import Fonts from 'unplugin-fonts/vite'
 import { defineConfig } from 'vite'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // Dla Rendera musi to być sam ukośnik (nie zmieniamy tego na nazwę repozytorium!)
+  base: '/', 
   plugins: [
-    Vue({
-      template: { transformAssetUrls },
+    vue({
+      template: { transformAssetUrls }
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    Vuetify({
-      autoImport: true,
-      styles: {
-        configFile: 'src/styles/settings.scss',
-      },
-    }),
-    Fonts({
-      fontsource: {
-        families: [
-          {
-            name: 'Roboto',
-            weights: [100, 300, 400, 500, 700, 900],
-            styles: ['normal', 'italic'],
-          },
-        ],
-      },
+    vuetify({
+      autoImport: true, // To naprawia błąd "e is not a function"!
     }),
   ],
-  define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('src', import.meta.url)),
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
-  },
-  server: {
-    port: 3000,
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
