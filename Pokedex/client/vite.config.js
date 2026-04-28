9a667e496ddfdbcc1a4c9197e8257a731f2228cf
Kutaphonix <shofertm@gmail.com>
2026-04-28 22:18:33 +0200

    Update vite.config.js

--- Pokedex/client/vite.config.js
+++ Pokedex/client/vite.config.js
@@ -1,12 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineConfig({
   base: '/', 
   
   plugins: [
-    vue()
+    vue({
+      template: { transformAssetUrls }
+    }),
+    vuetify({
+      autoImport: true,
+    }),
   ],
 
   resolve: {
@@ -16,10 +22,14 @@ export default defineConfig({
   },
 
   build: {
-    // TĄ LINIJKĄ WYŁĄCZAMY ZGNIATANIE KODU, KTÓRE PSUJE VUETIFY:
-    minify: false, 
+    // 1. Zmusza serwer do użycia nowoczesnego JS'a
+    target: 'esnext', 
+    
+    // 2. MAGIA: Tłumaczy problematyczne moduły (takie jak Axios) bez wywalania błędu __exportAll
+    commonjsOptions: {
+      transformMixedEsModules: true, 
+    },
     
     emptyOutDir: true,
-    assetsDir: 'assets',
   }
 })
