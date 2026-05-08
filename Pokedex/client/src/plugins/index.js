47a3cf24db3b3f9d118721beabf19cd4ad0950aa
Kamil Bilkiewicz <154635027+PSTorcik@users.noreply.github.com>
2026-05-08 19:02:07 +0200

    Remove TypeScript type imports from index.js
    Removed unused TypeScript type imports for JavaScript.

--- Pokedex/client/src/plugins/index.js
+++ Pokedex/client/src/plugins/index.js
@@ -5,10 +5,8 @@ import { createPinia } from 'pinia';
 // Plugins
 import vuetify from './vuetify'
 
-// Usunęliśmy "import type { App } from 'vue'" - w JS tego nie potrzebujemy
 
 export function registerPlugins (app) { 
- // Usunęliśmy ": App" - w JS nie definiujemy typów po dwukropku
  app.use(vuetify)
  app.use(createPinia());
  app.use(i18n);
