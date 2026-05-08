620a7f9e18446d4f30a1f440bdca950e54280625
Kutaphonix <shofertm@gmail.com>
2026-05-08 18:47:06 +0200

    Update main.js

--- Pokedex/client/src/main.js
+++ Pokedex/client/src/main.js
@@ -2,11 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { registerPlugins } from './plugins'
 
-const vuetify = createVuetify({
-  theme: {
-    defaultTheme: 'dark'
-  }
-})
+
 
 const app = createApp(App)
 
