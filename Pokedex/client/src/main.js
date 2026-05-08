b874aea2781022ccbedf741269281d2a01ecaee3
Kutaphonix <shofertm@gmail.com>
2026-05-08 18:38:19 +0200

    Add Vuetify with dark theme to main.js

--- Pokedex/client/src/main.js
+++ Pokedex/client/src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { registerPlugins } from './plugins'
 
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
+
 const app = createApp(App)
 
 registerPlugins(app)
